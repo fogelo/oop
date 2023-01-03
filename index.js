a529ae71307cd7d8466e8ddc48ce9304b421f936
fogelo <fogelo@yandex.ru>
2023-01-03 13:00:29 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//§ Процедурное программирование
+//Изначально все программы писались с процедурным подходом.
+const width = 5
+const height = 10
+
+const calcRectArea = (width, height) => {
+    return width * height
+}
+
+calcRectArea(width, height)
